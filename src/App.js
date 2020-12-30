8ade5f1f65c6fb43411e314ba0fc4c00420c6805
Lotuslinny <linny@fegg.nl>
2020-12-30 12:14:22 +0100

    exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+/*
 
-function App() {
+Challenge:
+1. Convert all 3 components to be class-based
+2. Fix the small bug
+*/
+
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+
+// #1
+/* function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Greeting />
     </div>
-  );
+  )
+}
+ */
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header username="vschool" />
+        <Greeting />
+      </div>
+    )
+  }
 }
 
-export default App;
+// #2
+/* function Header(props) {
+  return (
+    <header>
+      <p>Welcome, {props.username}!</p>
+    </header>
+  )
+} */
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <p>Welcome, {this.props.username}!</p>
+      </header>
+    )
+  }
+}
+
+// #3
+/* function Greeting() {
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else {
+    timeOfDay = "night"
+  }
+  return (
+    <h1>Good {timeOfDay} to you, sir or madam!</h1>
+  )
+}
+ */
+class Greeting extends Component {
+  render() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    if (hours < 12) {
+      timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon"
+    } else {
+      timeOfDay = "night"
+    }
+    return (
+      <h1>Good {timeOfDay} to you, sir or madam!</h1>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
+export default App
